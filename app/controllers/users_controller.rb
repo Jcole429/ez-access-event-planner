c46405a9597c7b385cefa77500e8cfe42985b02b
Jcole429 <Justincole29@hotmail.com>
2019-03-21 21:02:25 -0400

    logging out and more

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+  		log_in @user
   		flash[:success] = "Welcome to EZ - Access Event Planning!"
   		redirect_to @user
   	else
