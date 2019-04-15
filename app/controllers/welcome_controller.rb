cdca4eb50422a3f80eac8abb7b99ee7d82060d18
Jcole429 <Justincole29@hotmail.com>
2019-04-15 08:01:59 -0400

    navbar improvements

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,17 @@
 class WelcomeController < ApplicationController
+	def index
+		@title = "Home"
+	end
+
 	def about
+		@title = "About"
 	end
 
 	def contact
+		@title = "Contact Us"
 	end
 
 	def services
+		@title = "Services"
 	end
 end
