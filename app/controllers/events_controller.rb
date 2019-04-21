59929bcec0188162758214777497fb5de0133012
Jcole429 <Justincole29@hotmail.com>
2019-04-21 00:30:02 -0400

    fixed edit events bug

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,6 +30,14 @@ class EventsController < ApplicationController
 	end
 
 	def update
+		@event = Event.find(params[:id])
+
+		if @event.update_attributes(event_params)
+			flash[:success] = "Event updated"
+			redirect_to @event
+		else
+			render 'edit'
+		end
 	end
 
   	def destroy
